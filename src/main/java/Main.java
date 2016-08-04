4a747dab69eebfdceadc8b77e1e65c547adb62fa
aleksbocharov <sashok.san@gmail.com>
2016-08-04 18:24:48 +0200

    commit 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,21 +3,18 @@ import handlers.JsonHandler;
 import handlers.UrlHandler;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Scanner scan = new Scanner(System.in);
-        String cityName = scan.next();
-//        if(args.length>0 && args[0]!=null){
-        if(cityName.length()>0){
+        if(args.length>0 && args[0]!=null){
+            String cityName = args[0];
             String responseString = UrlHandler.getJsonData(cityName);
             System.out.println(responseString);
             JsonHandler jsonHandler = new JsonHandler(responseString);
             CsvWriter csvWriter = new CsvWriter(jsonHandler.parseJsonData());
-
+            csvWriter.writeToFile(cityName);
         }
         else{
             System.out.println("Error: empty request.");
