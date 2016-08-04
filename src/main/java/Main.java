dae99149072fd290ebc65059853264eb22c1359c
aleksbocharov <sashok.san@gmail.com>
2016-08-04 18:27:28 +0200

    commit 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
         if(args.length>0 && args[0]!=null){
             String cityName = args[0];
             String responseString = UrlHandler.getJsonData(cityName);
@@ -19,5 +20,6 @@ public class Main {
         else{
             System.out.println("Error: empty request.");
         }
+
     }
 }
